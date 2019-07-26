7c410ab270173dcd2891f29ba7922bb2c6b44949
Luke Bell <lukekingsleybell@gmail.com>
2019-07-26 12:05:29 +0200

    shows and artists.

--- app/static/js/index.js
+++ app/static/js/index.js
@@ -1,9 +1,18 @@
 $(document).ready(function() {
+  $("#enter").click(function(e) {
+    $("#section-web-home").hide();
+    $("#section-web-home").removeClass("section");
+    $("#home-nav").css("opacity", 1);
+    $(".section").first().addClass("active");
+    $(".nav-link").first().addClass("active");
+  });
+
   $("#home-link").click(function(e) {
     e.preventDefault();
     $(".section.active").removeClass("active");
     $(".nav-link.active").removeClass("active");
-    $("#section-web-home").addClass("active");
+    $(".section").first().addClass("active");
+    $(".nav-link").first().addClass("active");
   });
 
   $(".nav-link").click(function(e) {
